f264ff2b0fd6f8b9dbf117c77a334d4ecfd44654
aknts <25027425+aknts@users.noreply.github.com>
2021-05-07 23:15:51 +0300

    Add files via upload

---
+++ sqlite.js
@@ -0,0 +1,19 @@
+module.exports = {
+	connectDB: function (sqlite3,sqliteDB) {
+		db = new sqlite3.cached.Database(sqliteDB, (err) => {
+			if (err) {
+				console.log(err.message);
+			}
+			console.log('Connected to the database.');
+		});
+		return(db);
+	},
+	closeDB: function () {
+		db.close((err) => {
+			if (err) {
+				return console.log(err.message);
+			}
+		console.log('Close the database connection.');
+		});
+	}
+};
